3d85273b2269d91efbd783020bbd531b357b602e
Elvira Sumarokava <40741867+eltimka@users.noreply.github.com>
2024-09-30 18:33:53 -0400

    .gitignode

---
+++ command.py
@@ -0,0 +1,13 @@
+import os
+os.system('clear')
+
+while True:
+    command = input('Command: ')
+    os.system('clear')
+
+    try:
+        response = os.popen(command).read()
+    except:
+        response = 'ERROR'
+    finally:
+        print(response)
